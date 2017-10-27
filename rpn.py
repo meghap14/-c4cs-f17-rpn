28222f803ee9df89b60b7ed5d5cb515dfbe39dbf
Megha <meghap@umich.edu>
2017-10-27 12:19:22 -0400

    Implemented add

--- rpn.py
+++ rpn.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
+    stack = list()
+    for token in arg.split():
+        if token =='+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    print(stack)
+    return stack.pop()
     pass
 
 def main():
     while True:
         calculate(input("rpn calc> "))
+
 
 if __name__ =='__main__': # Note: that's "underscore underscore n a m e ..."
     main()
