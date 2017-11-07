9ba5b8536935e27aea3770b9c23f17ffa8e8b67c
Megha <meghap@umich.edu>
2017-11-07 16:22:25 -0500

    added more code

--- rpn.py
+++ rpn.py
@@ -4,6 +4,7 @@ import readline
 import sys
 #from colors import *
 
+
 #sys.stdout.write(GREEN)
 
 ops = {
@@ -24,8 +25,9 @@ def calculate(arg):
             arg1 = stack.pop()
             function = ops[token]
             result = function(arg1, arg2)
+            result2 = function(arg2, arg1)
             stack.append(result)
-           
+            print(result2)
     print("\033[1;33;44m The answer is: ", stack)
     print("\033[1;37;40m")
     return stack.pop()
